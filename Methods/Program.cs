7f8690847842791e6ae72240d33982e01a908ee0
MrPerson <diegoruffa@hotmail.com>
2023-02-08 21:08:32 -0300

    adding methods

---
+++ Methods/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Data;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            float[] pricesArray;
+
+            pricesArray = new float[10];
+
+            pricesArray[0] = 1.5f;
+            pricesArray[1] = 2.3f;
+            pricesArray[2] = 3f;
+            pricesArray[3] = 6f;
+            pricesArray[4] = 1.5f;
+
+            Random rnd = new Random();
+
+            Console.WriteLine("Hello world, of random numbers");
+            Console.WriteLine($"{rnd.Next(1, 10)}");
+        }
+    }
+}
