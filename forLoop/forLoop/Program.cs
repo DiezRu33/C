fc443d66b4da75afbe692670ededce1db1bcc956
MrPerson <diegoruffa@hotmail.com>
2023-02-16 17:42:50 -0300

    Adding for loops w/ C#

---
+++ forLoop/forLoop/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+// loop for execute the action for each loop;
+
+namespace ThisIsIt
+{
+    class ClassName
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Running... ");
+            System.Threading.Thread.Sleep(1000);
+
+            for(int i = 0; i < 11; i++)
+            {
+                if(i == 5)
+                {
+                    Console.WriteLine("The half is: " + i);
+                }
+                else
+                {
+                    Console.WriteLine("Now the number is: " + i);
+                }
+
+            }
+
+        }
+    }
+}
