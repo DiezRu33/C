b40fe32addded965048e08f5cbfea85b561d04fc
MrPerson <diegoruffa@hotmail.com>
2023-02-16 17:41:18 -0300

    Adding if sentences w/ C#

---
+++ ifSentens/ifSentens/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using System.Runtime.InteropServices;
+
+namespace NamespaceName
+{
+    class ClassName
+    {
+        static void Main(string[] args)
+        {
+            int anyvalue = 14;
+            string message = "";
+
+            if (anyvalue == 7)
+            {
+                message = "*I'm the message & the value is 7";
+            }else if(anyvalue == 14){
+                message = "The value is 14 too.";
+            }
+            else
+            {
+                Console.WriteLine("Not correct value");
+            }
+            Console.WriteLine($"\nThe message is:{message}\n");
+
+            Console.WriteLine($"Running... ");
+        }
+    }
+}
